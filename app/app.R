2e455cfb42f60fa4be84e01e6fbcc592037099eb
Hengshan <hengshan.li@gmail.com>
2019-02-28 13:45:56 +0800

    Test Git

--- app/app.R
+++ app/app.R
@@ -202,14 +202,6 @@ server <- function(input, output, session) {
   })
 
   
-  output$plot <- renderPlot({
-    plot(cars, type=input$plotType)
-  })
-  
-  output$summary <- renderPrint({
-    summary(cars)
-  })
-  
 }
 
 shinyApp(ui, server)
